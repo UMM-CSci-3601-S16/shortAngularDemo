46a08f4470add7482459f0a6237a2c137419c6ae
Joseph Thelen <joethe@users.noreply.github.com>
2016-02-01 14:44:10 -0600

    added "remaining" function

--- todo.controller.js
+++ todo.controller.js
@@ -14,4 +14,10 @@ angular.module("main").controller("todoController", function() {
     todoList.todos.push({item: todoList.todoText, done: false});  // put the new item in the list
     todoList.todoText = ''; // resetting the input field
   };
+  
+  todoList.remaining = function() {
+    var count = 0;
+    angular.forEach(todoList.todos, function(todo) { count += todo.done ? 0 : 1; });
+    return count;
+  };
 });
